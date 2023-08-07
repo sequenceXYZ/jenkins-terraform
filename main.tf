0d57857ad40c995628399a46991325686fc33326
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2023-08-07 09:35:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "TF_instance" {
   ami           = "ami-04e601abe3e1a910f"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_instance"
